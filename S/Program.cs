42e4cbba9a2dd9f3e97f14f428008a73b32c8453
John Nooney <1803534@uad.ac.uk>
2024-02-10 12:16:36 +0000

    Remove old idea for SRP

--- S/Program.cs
+++ S/Program.cs
@@ -6,22 +6,13 @@ class Program
 { 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello");
-        Console.WriteLine("Starting Spinners...");
+        OutputHandler outputHandler = new OutputHandler();
+        InputValidator inputValidator = new InputValidator();
+        InputHandler inputHandler = new InputHandler(inputValidator);
 
-        Spinner spinner = new Spinner(0, 3);
-        Spinner spinner2 = new Spinner(1, 3);
-        Spinner spinner3 = new Spinner(2, 3);
-        
-        spinner.start();
-        spinner2.start();
-        spinner3.start();
+        outputHandler.outputString("Enter something!");
+        inputHandler.collectStringInput();
 
-        Thread.Sleep(10000);
-
-        spinner.stop();
-        spinner2.stop();
-        spinner3.stop();
-        
+        outputHandler.outputString(inputHandler.getStringInput());        
     }
 }
--- S/README.md
+++ S/README.md
@@ -1,10 +1,14 @@
 # Single Responsibility Principle
 This principle states a module should only have one responsibility.
 
-The program to demonstrate this is a simple CLI-based slot machine. This program will handle the following operations:
-- User input (pull lever, select bet)
-- Random number generation for reels
-- Symbol matching logic
-- Payout calculation
-- Displaying results
+## Program Requirements
+* CLI program 
+* Outputs a prompt
+* Collects user input
+    * input is validated for being null or empty
+    * if validation fails prompt user to re-try input
+* Prints user input back
 
+
+## Implementation Details
+From an implementation perspective, this program will demonstrate the Single Responsibility Principle through the use of classes and methods.
