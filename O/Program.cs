bef85b3b43727680bd4fc62b3df8bd6c19830d63
John Nooney <1803534@uad.ac.uk>
2024-02-11 19:36:44 +0000

    Refactor runner to use builder

---
+++ O/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace OpenClosePrinciple;
+
+class Program
+{
+    private static InputOutput stringIO;
+    private static InputOutputBuilder ioBuilder;
+    private static InputOutputDirector ioDirector;
+    private static Output consoleOutput;
+
+    static Program()
+    {
+        initIO();
+        createStringIO();
+    }
+
+    static void Main(string[] args)
+    {
+        consoleOutput.StringOutput = "Hello There";
+
+        stringIO.Output(consoleOutput);
+        stringIO.Input();
+        stringIO.EchoInput();
+    }
+
+    private static void initIO()
+    {
+        ioBuilder = new InputOutputBuilder();
+        ioDirector = new InputOutputDirector
+        {
+            builder = ioBuilder
+        };
+        
+        consoleOutput = new Output();
+    }
+
+    private static void createStringIO()
+    {
+        ioDirector.BuildStringIO();
+
+        stringIO = ioBuilder.GetInputOutput();
+    }
+}
