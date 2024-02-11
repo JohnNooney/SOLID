d7cd7a59ae526916453768c3f9b3ccd53552971a
John Nooney <1803534@uad.ac.uk>
2024-02-11 19:37:36 +0000

    Refactor validator for different types

---
+++ O/src/InputValidator.cs
@@ -0,0 +1,25 @@
+namespace OpenClosePrinciple;
+
+using System;
+
+public class InputValidator
+{
+    private bool validity;
+    public bool isStringValid(Input input)
+    {
+        validity = !string.IsNullOrEmpty(input.StringInput);
+        
+        if(!validity)
+        {
+            Console.WriteLine("String Input is invalid. Pleas try again.");
+        }
+
+        return validity;
+    }
+
+    public bool isIntValid(Input input)
+    {
+        // TODO
+        return validity;
+    }
+}
