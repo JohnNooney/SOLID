7e69f8aa7e8c68edf034fe04e3c20ca93f009085
John Nooney <1803534@uad.ac.uk>
2024-02-11 19:35:12 +0000

    Add Input and Output Types

---
+++ O/src/Output.cs
@@ -0,0 +1,10 @@
+namespace OpenClosePrinciple;
+
+public class Output
+{
+    public string? StringOutput{ get; set;}
+
+    public int? IntOutput{ get; set;}
+
+    public double? DoubleOutput{ get; set;}
+}
