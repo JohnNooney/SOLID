c012e36e49f8f4c3ed90c9827edb23a01c29f6fe
John Nooney <1803534@uad.ac.uk>
2024-02-19 09:14:48 +0000

    Refactor into common dir

--- L/LiskovSubstitutionPrinciple/src/shapes/Rectangle.cs
+++ L/LiskovSubstitutionPrinciple/src/shapes/Rectangle.cs
@@ -5,7 +5,7 @@ class Rectangle : Shape
     protected int dimension2;
 
     public override void SetDimensions(params int[] dimensions)
-    {
+
         dimension = dimensions[0];
         dimension2 = dimensions[1];
     }
