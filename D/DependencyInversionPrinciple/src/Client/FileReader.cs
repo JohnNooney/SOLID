016c6ddc734e668439135ed32090a5fa349d1998
John Nooney <1803534@uad.ac.uk>
2024-02-23 10:05:51 +0000

    Add FileReader implementation

--- D/DependencyInversionPrinciple/src/Client/FileReader.cs
+++ D/DependencyInversionPrinciple/src/Client/FileReader.cs
@@ -1,6 +1,59 @@
 namespace DependencyInversionPrinciple;
 
+using System.IO;
+
+// Reads file from a specified directory location
+// and once read, copies files from source to destination
 class FileReader
 {
-    // Use the IO stream to write to a file
+    private string sourcePath;
+    private string destinationPath;
+
+    public FileReader(string sourcePath, string destinationPath)
+    {
+        this.sourcePath = sourcePath;
+        this.destinationPath = destinationPath;
+    }
+
+    public void ReadFile(string fileName)
+    {
+        readFromSource(fileName);
+        copyToDestination(fileName);
+    }
+
+    private void readFromSource(string fileName)
+    {
+        Console.WriteLine($"Reading file: {fileName}");
+
+        try
+        {
+            using(StreamReader reader = new StreamReader(sourcePath + fileName))
+            {
+                string line = reader.ReadLine();
+
+                while(line != null) 
+                {
+                    Console.WriteLine(line);
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Error while reading {fileName}: {ex.Message}");
+        }
+    }
+
+    private void copyToDestination(string fileName)
+    {
+        try
+        {
+            File.Copy(sourcePath + fileName, destinationPath + fileName, true);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Error while copying {fileName}: {ex.Message}");
+        }
+    }
 }
