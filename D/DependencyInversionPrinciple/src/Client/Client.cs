ca5322e0ad670b51bb3214483a6b081158f5d135
John Nooney <1803534@uad.ac.uk>
2024-02-23 07:50:46 +0000

    Add client boilerplate

---
+++ D/DependencyInversionPrinciple/src/Client/Client.cs
@@ -0,0 +1,7 @@
+namespace DependencyInversionPrinciple;
+
+class Client
+{
+    // Client will create a subscriber and Read files
+    // files will be deleted after read
+}
---
+++ D/DependencyInversionPrinciple/src/Client/FileReader.cs
@@ -0,0 +1,6 @@
+namespace DependencyInversionPrinciple;
+
+class FileReader
+{
+    // Use the IO stream to write to a file
+}
